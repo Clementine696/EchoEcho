15c57df1d998b13dca62e900a83832a56974ebcb
Nongpeamm <Beambike2@gmail.com>
2023-01-10 20:18:20 +0700

    Master volume update
    อธิบายการทำงานเพิ่มเติม + บอก scale

--- Backend/MicCapBeam.py
+++ Backend/MicCapBeam.py
@@ -8,19 +8,40 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-# volume.SetMasterVolumeLevel(-1, None)
-# volume.SetMute(0, None)
+# หาก SetMasterVolumeLevel  = 0.0 volume จะเป็น 100 และ SetMasterVolumeLevel  = -61 (65.25) volume จะเป็น 0
+# volume.SetMasterVolumeLevel(-61, None)
+# volume.SetMasterVolumeLevel(0, None)
+
+#SetMute ลำโพง หากเป็น 0 เปิดลำโพง และเป็น 1 จะปิดลำโพง 
+volume.SetMute(0, None)
+# volume.SetMute(1, None)
 
 current = volume.GetMasterVolumeLevel()
-volume.SetMasterVolumeLevel(current + 6.0, None)
+print("Current Value : ", current )
+# หากจะปรับ volume ที่ละ 1 ให้ปรับตวามต่างไปที่ 0.151 หรือ -0.15066957473754883
+
+# ปรับ volume ลง
+volume.SetMasterVolumeLevel(current - 0.15066957473754883, None)
+
+# ปรับ volume ขึ้น
+# volume.SetMasterVolumeLevel(current + 0.151, None)
+
+# หากจะปรับ volume ที่ละ 5 ให้ปรับตวามต่างไปที่ 0.755 หรือ -0.7533478736877442
+
+# ปรับ volume ลง
+# volume.SetMasterVolumeLevel(current - 0.7533478736877442, None)
+
+# ปรับ volume ขึ้น
+# volume.SetMasterVolumeLevel(current + 0.755, None)
+
 
 sessions = AudioUtilities.GetAllSessions()
 
 for session in sessions:
     volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-    # print (session.Process.name())
-    if session.Process and session.Process.name() == "brave.exe":
-        volume.SetMasterVolume(0.5 ,None)
+    print (session.Process.name())
+    # if session.Process and session.Process.name() == "brave.exe":
+    #     volume.SetMasterVolume(0.5 ,None)
     
 
 
