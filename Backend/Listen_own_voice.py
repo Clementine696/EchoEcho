550fd6f0faffc2fd57929313a9f223ca0b62274f
Nongpeamm <Beambike2@gmail.com>
2023-01-11 21:57:23 +0700

    Test own voice
    ดึงเสียงจากไมค์ออกมาใช้ได้เลย

---
+++ Backend/Listen_own_voice.py
@@ -0,0 +1,28 @@
+# https://stackoverflow.com/questions/510357/how-to-read-a-single-character-from-the-user
+
+import sounddevice as sd
+import numpy as np
+import time
+import keyboard
+import os
+
+def callback(indata, outdata, frames, time, status):
+    if status:
+        print(status)
+    outdata[:] = indata
+
+def main():
+    with sd.Stream(samplerate=44100, blocksize=1024, dtype=np.float32,
+                    channels=2, callback=callback):
+        while True:
+            if keyboard.is_pressed('q'):
+                quit()
+                
+            # ! ไม่เข้าใจว่า time.sleep 1000 กับ  0 มีผลต่างกันอย่างไร
+            time.sleep(0)
+            
+if __name__ == '__main__':
+    main()
+    if keyboard.is_pressed('q'):
+                quit()
+    
