a49683722cff8cf40d4d9a5e82e2f7d3fc370122
Cℓementine♥ <kanate--@hotmail.com>
2023-01-14 21:43:08 +0700

    Play To Mic

---
+++ Backend/PlayToMic.py
@@ -0,0 +1,31 @@
+import pyaudio
+import wave
+
+# initialize PyAudio
+p = pyaudio.PyAudio()
+
+# open a stream for playing audio
+stream = p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=44100,
+                output=True,
+                frames_per_buffer=1024)
+
+# open the audio file to be played
+wf = wave.open("sound/StarWars60.wav", "rb")
+
+# start playing the audio
+
+stream.start_stream()
+while stream.is_active():
+    data = wf.readframes(1024)
+    stream.write(data)
+    if len(data) == 0:
+        break
+
+# stop the stream
+stream.stop_stream()
+stream.close()
+
+# close PyAudio
+p.terminate()
