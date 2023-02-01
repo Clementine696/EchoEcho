2fb66d4ce2333c035582a589b3adcea8812d3fb5
Gamchu <phutthiphong.sae@gmail.com>
2023-02-01 21:06:27 +0700

    Edit conflict

--- Frontend/app01/lib/widgets/pages/microphone_page.dart
+++ Frontend/app01/lib/widgets/pages/microphone_page.dart
@@ -24,58 +24,63 @@ class MicrophonePage extends StatelessWidget {
               height: 400,
               child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      shadowColor: AppColors.blackColor,
-                      shape: RoundedRectangleBorder(
+                      shape: const RoundedRectangleBorder(
                           borderRadius: BorderRadius.only(
                               topLeft: Radius.circular(25),
                               bottomLeft: Radius.circular(25)),
                           side: BorderSide(color: AppColors.blackColor)),
+                      foregroundColor: const Color.fromARGB(255, 3, 253, 240),
+                      // backgroundColor: Color.fromARGB(47, 21, 227, 241),
                       primary: AppColors.secDarkerColor),
                   onPressed: () {
                     debugPrint("Noise Suppression");
                   },
                   child: const Text.rich(TextSpan(children: <TextSpan>[
                     TextSpan(
-                        text: "Noise Suppression\n",
+                        text: "Noise Suppression\n\n",
                         style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 30)),
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30)),
                     TextSpan(
                         text:
                             "detecting the sound coming into the headset, and generating signals that are out-of-phase with the offending signals, cancelling them out.",
                         style: TextStyle(
-                            fontWeight: FontWeight.w100,
-                            fontSize: 20,
-                            color: AppColors.grayDarkColor))
+                            color: Color.fromARGB(255, 117, 115, 115),
+                            fontWeight: FontWeight.normal,
+                            fontSize: 20))
                   ]))),
             ),
             SizedBox(
-              width: 389,
-              height: 400,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(25),
-                              bottomRight: Radius.circular(25))),
-                      side: BorderSide(color: AppColors.blackColor),
-                      primary: AppColors.secDarkerColor),
-                  onPressed: () {
-                    debugPrint("Echo Cancallation");
-                  },
-                  child: const Text.rich(TextSpan(children: <TextSpan>[
-                    TextSpan(
-                        text: "Echo Cancellation\n",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 30)),
-                    TextSpan(
-                        text:
-                            "methods used in telephony to improve voice quality by preventing echo from being created or removeing it after it is already present.",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w100,
-                            fontSize: 20,
-                            color: AppColors.grayDarkColor))
-                  ]))),
-            )
+                width: 389,
+                height: 400,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: const Color.fromARGB(255, 3, 253, 240),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(25),
+                                bottomRight: Radius.circular(25))),
+                        side: const BorderSide(color: AppColors.blackColor),
+                        primary: AppColors.secDarkerColor),
+                    onPressed: () {
+                      debugPrint("Echo Cancallation");
+                    },
+                    child: const Text.rich(TextSpan(children: <TextSpan>[
+                      TextSpan(
+                          text: "Echo Cancallation\n\n",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30)),
+                      TextSpan(
+                          text:
+                              "methods used in telephony to improve voice quality by preventing echo from being created or removing it after it is already present.",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 117, 115, 115),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20))
+                    ]))))
           ],
         ),
         SizedBox(
@@ -83,46 +88,17 @@ class MicrophonePage extends StatelessWidget {
             height: 82,
             child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                    alignment: Alignment(-1, 0),
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(25)),
                     primary: AppColors.secDarkerColor),
                 onPressed: () {
                   debugPrint("Test mic");
                 },
-                child: const Testmicicon())),
+                child: const Text(
+                  "Test microphone",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
+                ))),
       ],
     );
-  }
-}
-
-class Testmicicon extends StatefulWidget {
-  const Testmicicon({super.key});
-
-  @override
-  State<Testmicicon> createState() => _TestmiciconState();
-}
-
-class _TestmiciconState extends State<Testmicicon> {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: 380,
-        child: Container(
-          padding: EdgeInsets.only(left: 15),
-          child: Row(
-            children: [
-              Text(
-                "Test microphone",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w100),
-              ),
-              //    Icon(
-              //     Icons.square,
-              //     color: AppColors.whiteColor,
-              //    size: 40,
-              //   ),
-            ],
-          ),
-        ));
   }
 }
