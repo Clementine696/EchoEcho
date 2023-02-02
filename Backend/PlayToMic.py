975c5882fc6dbf82860b3fef72f619f2d0340f4b
Cℓementine♥ <kanate--@hotmail.com>
2023-02-02 23:01:36 +0700

    Virtual Mic

--- Backend/PlayToMic.py
+++ Backend/PlayToMic.py
@@ -1,5 +1,12 @@
 import pyaudio
 import wave
+import sounddevice as sd
+
+device_list = sd.query_devices()
+for i in (device_list):
+    if "CABLE Input " in i['name']:
+        vb_index = i['index']
+        break
 
 # initialize PyAudio
 p = pyaudio.PyAudio()
@@ -9,7 +16,8 @@ stream = p.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=44100,
                 output=True,
-                frames_per_buffer=1024)
+                frames_per_buffer=1024,
+                output_device_index=vb_index)
 
 # open the audio file to be played
 # wf = wave.open("Backend/sound/StarWars60.wav", "rb")
