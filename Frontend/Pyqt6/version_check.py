83f46df4c3fb56ede23e3b89760544ffede067f2
Gamchu <phutthiphong.sae@gmail.com>
2023-02-06 00:42:48 +0700

    Start Ui

---
+++ Frontend/Pyqt6/version_check.py
@@ -0,0 +1,16 @@
+# PyQt6 Version check
+from PyQt6.QtCore import QT_VERSION_STR
+print("PyQt6 version:",QT_VERSION_STR)
+
+#PySide6 Version check
+import PySide6
+print("PySide6 version:", PySide6.__version__)
+
+#Python version check
+import sys
+print("Python version:", sys.version)
+
+#Windows Version check
+import platform
+print("Windows version:", platform.platform())
+
