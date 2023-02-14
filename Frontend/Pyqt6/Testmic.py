4629607c2176a1ed9a5535bfe2b687a9886cfffc
Cℓementine♥ <kanate--@hotmail.com>
2023-02-15 01:54:47 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/Clementine696/EchoEcho""
    This reverts commit 9677251683d588b1dfb0f94bb06d9468828722da.

--- Frontend/Pyqt6/Testmic.py
+++ Frontend/Pyqt6/Testmic.py
@@ -3,10 +3,10 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDesktopWidget
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
-import noisereduce as nr
-import pyaudio
-import numpy as np
-import sounddevice as sd
+# import noisereduce as nr
+# import pyaudio
+# import numpy as np
+# import sounddevice as sd
 
 
 class App(QWidget):
@@ -34,37 +34,37 @@ class App(QWidget):
     @pyqtSlot()
     def on_click(self):
         print('PyQt5 button click')
-        p = pyaudio.PyAudio()
+    #     p = pyaudio.PyAudio()
 
-        # Open the input microphone stream
-        input_stream = p.open(format=pyaudio.paInt16,
-                          channels=1,
-                          rate=44100,
-                          input=True,
-                          frames_per_buffer=1024)
+    #     # Open the input microphone stream
+    #     input_stream = p.open(format=pyaudio.paInt16,
+    #                       channels=1,
+    #                       rate=44100,
+    #                       input=True,
+    #                       frames_per_buffer=1024)
 
 
-        # Open the output microphone stream
-        output_stream = p.open(format=pyaudio.paInt16,
-                       channels=1,
-                       rate=44100,
-                       output=True)
+    #     # Open the output microphone stream
+    #     output_stream = p.open(format=pyaudio.paInt16,
+    #                    channels=1,
+    #                    rate=44100,
+    #                    output=True)
 
-        while True:
-    # Read input microphone data
-            input_data = input_stream.read(1024)
-    # Perform noise reduction
-            audio_frame = np.frombuffer(input_data, dtype=np.int16)
-            reduced_noise = nr.reduce_noise(audio_frame, sr=44100)
-            audio_data = reduced_noise.tobytes()
-    # Write the audio data to the output microphone stream
-            output_stream.write(audio_data)
+    #     while True:
+    # # Read input microphone data
+    #         input_data = input_stream.read(1024)
+    # # Perform noise reduction
+    #         audio_frame = np.frombuffer(input_data, dtype=np.int16)
+    #         reduced_noise = nr.reduce_noise(audio_frame, sr=44100)
+    #         audio_data = reduced_noise.tobytes()
+    # # Write the audio data to the output microphone stream
+    #         output_stream.write(audio_data)
 
-        input_stream.stop_stream()
-        input_stream.close()
-        output_stream.stop_stream()
-        output_stream.close()
-        p.terminate()   
+    #     input_stream.stop_stream()
+    #     input_stream.close()
+    #     output_stream.stop_stream()
+    #     output_stream.close()
+    #     p.terminate()   
 
 
 if __name__ == '__main__':
