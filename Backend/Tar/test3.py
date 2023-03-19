37e143aef994c6967dc1ea34c5fc6e95b6ef982e
TtB00 <88588878+TtB00@users.noreply.github.com>
2023-03-19 16:25:56 +0700

    test

--- Backend/Tar/test3.py
+++ Backend/Tar/test3.py
@@ -12,22 +12,17 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # เรียกใช้ฟังก์ชันเพิ่ม widget
         self.initUI()
 
-        # อ่านข้อมูลของไฟล์เสียงจากไฟล์ .txt และแสดงในตาราง
         self.loadFiles()
 
-        # บันทึกข้อมูลลงไฟล์ .txt เมื่อโปรแกรมถูกปิด
         app = QApplication.instance()
         app.aboutToQuit.connect(self.saveFiles)
 
     def initUI(self):
-        # สร้างหน้าต่างหลัก
         self.setWindowTitle('Audio File Recorder')
         self.setGeometry(100, 100, 800, 600)
 
-        # สร้าง widget
         self.central_widget = QWidget()
         self.central_layout = QGridLayout()
         self.central_widget.setLayout(self.central_layout)
@@ -40,15 +35,12 @@ class App(QMainWindow):
         self.table.setHorizontalHeaderLabels(['File Name', 'Duration', 'Play', 'Delete'])
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        # เพิ่ม widget ลงใน layout
         self.central_layout.addWidget(self.add_file_button, 0, 0)
         self.central_layout.addWidget(self.table, 1, 0)
 
-        # เรียกใช้ central widget
         self.setCentralWidget(self.central_widget)
 
     def addFile(self):
-        # เปิด dialog เพื่อให้เลือกไฟล์เสียง
         file_dialog = QFileDialog()
         file_dialog.setNameFilter('Audio Files (*.wav *.mp3)')
         file_dialog.setFileMode(QFileDialog.ExistingFile)
@@ -57,36 +49,29 @@ class App(QMainWindow):
             file_names = file_dialog.selectedFiles()
             for file_name in file_names:
                 if os.path.splitext(file_name)[1] in ('.wav', '.mp3'):
-                # หา duration ของไฟล์เสียง
                     duration = self.getDuration(file_name)
 
-                # เพิ่มข้อมูลลงในตาราง
                     row_position = self.table.rowCount()
                     self.table.insertRow(row_position)
                     self.table.setItem(row_position, 0, QTableWidgetItem(os.path.basename(file_name)))
                     self.table.setItem(row_position, 1, QTableWidgetItem(duration))
 
-                # สร้างเครื่องเล่นเสียงสำหรับไฟล์นี้
                     player = QMediaPlayer()
                     content = QMediaContent(QUrl.fromLocalFile(file_name))
                     player.setMedia(content)
 
-                # สร้างปุ่มเล่นเสียง
                     play_button = QPushButton('Play')
                     play_button.setObjectName(file_name)
                     play_button.clicked.connect(lambda _, b=file_name, p=player: self.playAudio(b, p))
                     self.table.setCellWidget(row_position, 2, play_button)
 
-                # สร้างปุ่มลบไฟล์
                     delete_button = QPushButton('Delete')
                     delete_button.clicked.connect(lambda state, row=row_position: self.deleteFile(row))
                     self.table.setCellWidget(row_position, 3, delete_button)
 
-                # บันทึกชื่อไฟล์และ duration ลงในไฟล์ .txt
                     self.saveFiles()
 
     def loadFiles(self):
-        # อ่านไฟล์ .txt เพื่อแสดงข้อมูลของไฟล์เสียงที่เคยบันทึกไว้
         try:
             with open('soundpad.txt', 'r') as f:
                 lines = f.readlines()
@@ -97,18 +82,15 @@ class App(QMainWindow):
                     self.table.setItem(row_position, 0, QTableWidgetItem(os.path.basename(file_name)))
                     self.table.setItem(row_position, 1, QTableWidgetItem(duration))
 
-                    # สร้างเครื่องเล่นเสียงสำหรับไฟล์นี้
                     player = QMediaPlayer()
                     content = QMediaContent(QUrl.fromLocalFile(file_name))
                     player.setMedia(content)
 
-                    # สร้างปุ่มเล่นเสียง
                     play_button = QPushButton('Play')
                     play_button.setObjectName(file_name)
                     play_button.clicked.connect(lambda _, b=file_name, p=player: self.playAudio(b, p))
                     self.table.setCellWidget(row_position, 2, play_button)
 
-                    # สร้างปุ่มลบไฟล์
                     delete_button = QPushButton('Delete')
                     delete_button.clicked.connect(lambda state, row=row_position: self.deleteFile(row))
                     self.table.setCellWidget(row_position, 3, delete_button)
@@ -118,40 +100,23 @@ class App(QMainWindow):
         except Exception as e:
             print("Error loading audio files:",e)
 
-
     def deleteFile(self, row):
-    # ลบข้อมูลออกจากตาราง
         self.table.removeRow(row)
 
-    # ลบข้อมูลออกจากไฟล์ .txt
         with open('soundpad.txt', 'r') as f:
             lines = f.readlines()
         with open('soundpad.txt', 'w') as f:
             for i, line in enumerate(lines):
                 if i != row:
                     f.write(line)
-
-    
+        
     def playAudio(self, file_name, player):
-        if player.state() == QMediaPlayer.PlayingState:
+        if  player.mediaStatus() == QMediaPlayer.LoadedMedia:
             player.stop()
 
         content = QMediaContent(QUrl.fromLocalFile(file_name))
         player.setMedia(content)
         player.play()
-
-    # def createPlayer(self, row, file_name):
-    #     # สร้างเครื่องเล่นเสียงสำหรับไฟล์ที่เพิ่มเข้ามา
-    #     player = QMediaPlayer()
-    #     content = QMediaContent(QUrl.fromLocalFile(os.path.join(self.directory_path, file_name)))
-    #     player.setMedia(content)
-    #     play_button = QPushButton('Play')
-    #     play_button.clicked.connect(lambda state, x=row: self.playFile(x))
-    #     delete_button = QPushButton('Delete')
-    #     delete_button.clicked.connect(self.deleteFile)
-    #     self.table.setCellWidget(row, 2, play_button)
-    #     self.table.setCellWidget(row, 3, delete_button)
-    #     self.players.append(player)
 
     def getDuration(self, file_name):
         if file_name.endswith('.mp3'):
@@ -163,7 +128,6 @@ class App(QMainWindow):
         return time.strftime('%H:%M:%S', time.gmtime(duration))
     
     def saveFiles(self):
-    # บันทึกข้อมูลลงในไฟล์ .txt
         with open('soundpad.txt', 'w') as f:
             for row in range(self.table.rowCount()):
                 file_name = self.table.item(row, 0).text()
