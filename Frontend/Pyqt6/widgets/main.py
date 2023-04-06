783b7e65073e7658e9bc2bd18a875dcc45bfd7b1
Cℓementine♥ <kanate--@hotmail.com>
2023-04-06 18:58:32 +0700

    VoiceChanger

--- Frontend/Pyqt6/widgets/main.py
+++ Frontend/Pyqt6/widgets/main.py
@@ -14,6 +14,22 @@ import noisereduce as nr
 from math import sqrt
 from pydub import AudioSegment,generators
 
+#Voice Changer
+import librosa
+# define the pitch shift factor
+pitch_shift_factor = 2 # double the pitch
+bins = 12
+hz = 0
+# calculate the pitch shift amount in semitones
+pitch_shift_amount = librosa.hz_to_midi(pitch_shift_factor * librosa.midi_to_hz(hz))
+import scipy.signal as signal
+# define filter parameters
+cutoff_freq = 4000  # Hz
+filter_order = 10
+# create the filter
+nyquist_freq = 0.5 * 48000
+b, a = signal.butter(filter_order, cutoff_freq/nyquist_freq, btype='lowpass')
+
 # IMPORT GUI FILE
 from ui_main import *
 
@@ -200,7 +216,16 @@ class SoundSystem():
                 output_sound = self.numpy_data.tostring()
 
             if(Voice_changer_state):
-                print('change')
+                numpy_data = np.fromstring(audio_data, dtype=np.int16)
+                numpy_data_float = numpy_data.astype('float32') / 32767.0 # scale to range [-1, 1]
+                # pitch shift the audio data
+                pitch_shifted_data = librosa.effects.pitch_shift(numpy_data_float, 48000, n_steps=pitch_shift_amount, bins_per_octave=bins)
+                # apply the filter to the pitch shifted data
+                filtered_data = signal.filtfilt(b, a, pitch_shifted_data)
+                # convert the pitch shifted data to a string for playback
+                output_sound = (pitch_shifted_data * 32767).astype(np.int16).tostring()
+                # output_sound = pitch_shifted_data.astype(np.int16).tostring()
+                output_sound = (pitch_shifted_data * 32767).astype(np.int16).tostring()
 
             self.virtual_microphone_stream.write(output_sound)
 
