90d188ea10cfac0e6cbf347f426b14dcfdbcb19c
Nongpeamm <meesul_kantinan@hotmail.com>
2023-05-07 00:14:07 +0700

    create database

--- Web/app.py
+++ Web/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
+# import sqlite3
+
+# con = sqlite3.connect("Audio_DB.db")
+
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
 
 @app.route('/')
 def Home():
@@ -12,6 +19,10 @@ def tutorial():
 @app.route('/voicechanger')
 def voicechanger():
     return render_template('voicechanger.html')
+
+# @app.route('/voicechanger')
+# def addAudio():
+#     return render_template('voicechanger.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
