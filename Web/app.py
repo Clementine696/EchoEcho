5aeaa9c02be2a625755429b9dd0c6f59fe878afb
Dreamer1176 <98012273+Dreamer1176@users.noreply.github.com>
2023-05-08 20:58:44 +0700

    madal ยังไม่ตกแต่ง

--- Web/app.py
+++ Web/app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # db = SQLAlchemy(app)
 
+POSTS = ['hello world', 'how are you', 'you good fam']
+
 @app.route('/')
 def Home():
     return render_template('home.html')
@@ -16,13 +18,18 @@ def Home():
 def tutorial():
     return render_template('tutorial.html')
 
-@app.route('/voicechanger')
+@app.route('/Soundpad')
 def voicechanger():
-    return render_template('voicechanger.html')
+    return render_template('Soundpad.html',posts = POSTS )
 
 # @app.route('/voicechanger')
 # def addAudio():
 #     return render_template('voicechanger.html')
+
+@app.route('/modal')
+def modal():
+    return render_template('modal.html', posts = POSTS)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
