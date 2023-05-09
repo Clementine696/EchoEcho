dca051af1dd2fea12bff1481f8e93d7e2fb78262
Nongpeamm <Beambike2@gmail.com>
2023-05-09 22:55:31 +0700

    delete unuse fiile

--- Web/app.py
+++ Web/app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
-# import sqlite3
-
-# con = sqlite3.connect("Audio_DB.db")
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# db = SQLAlchemy(app)
 
 POSTS = ['hello world', 'how are you', 'you good fam']
 
