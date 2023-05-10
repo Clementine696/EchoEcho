25bc781fe30a7abadb00e9d049aa6fdb7a0498de
Nongpeamm <Beambike2@gmail.com>
2023-05-11 00:33:07 +0700

    upload/dowload audio file

--- Web/app.py
+++ Web/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, send_file, jsonify
+import base64
+import sqlite3
+
+# เชื่อมต่อ database
+db_local = "Audio_DB.db"
 
 app = Flask(__name__)
-
-POSTS = ['hello world', 'how are you', 'you good fam']
 
 @app.route('/')
 def Home():
@@ -14,7 +17,13 @@ def tutorial():
 
 @app.route('/Soundpad')
 def voicechanger():
-    return render_template('Soundpad.html',posts = POSTS )
+    conn = sqlite3.connect(db_local)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, audio_name, duration FROM Soundpad")
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('soundpad.html', audio_files=data)
+    # return render_template('soundpad.html')
 
 # @app.route('/voicechanger')
 # def addAudio():
@@ -22,8 +31,38 @@ def voicechanger():
 
 @app.route('/modal')
 def modal():
-    return render_template('modal.html', posts = POSTS)
+    return render_template('modal.html')
 
+@app.route('/addAudio', methods=['GET','POST'])
+def addAudio():
+    if request.method == 'POST':
+        audio_name = request.form['audio_name']
+        audio_src = request.form['audio_src']
+        duration = request.form['duration']
+        
+        conn = sqlite3.connect(db_local)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Soundpad (audio_name, audio_src, duration) VALUES (?, ?, ?)", (audio_name, audio_src, duration))
+        conn.commit()
+        conn.close()
+        
+    return "Add Audio Success!"
 
+@app.route('/download/<int:index>')
+def download(index):
+    print("index: ", index)
+    conn = sqlite3.connect(db_local)
+    cursor = conn.cursor()
+    cursor.execute("SELECT audio_name, audio_src FROM Soundpad WHERE id=?", (index,))
+    data = cursor.fetchone()
+    conn.close()
+    audio_name = data[0]
+    audio_src = data[1]
+    print("audio_name: ", audio_name)
+    try:
+        return jsonify({'audio_name': audio_name, 'audio_src': audio_src})
+    except Exception as e:
+        return str(e)
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- Web/test.py
+++ Web/test.py
@@ -5,15 +5,6 @@ db_local = "Audio_DB.db"
 connect = sqlite3.connect(db_local)
 cursor = connect.cursor()
 
-# cursor.execute('''
-#     CREATE TABLE IF NOT EXISTS Soundpad (
-#         ID INTEGER PRIMARY KEY AUTOINCREMENT,
-#         Audio_name TEXT,
-#         Audio_file TEXT,
-#         Duration FLOAT
-#     )
-# ''')
-
 cursor.execute("INSERT INTO Soundpad (Audio_name, Audio_file, Duration) VALUES (?, ?, ?)", ('MRE', 'MRE.mp3', 0.10))
 connect.commit()
 connect.close()
